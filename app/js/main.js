8f51744232774ed6088ccf200fd31e33be0172e6
Oleg <server90@mail.ru>
2021-05-31 01:00:47 +0300

    Вторая секция готова

--- app/js/main.js
+++ app/js/main.js
@@ -17,5 +17,30 @@ $(function () {
     focusOnSelect: true
   });
 
-  
+  $('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left animate__animated animate__fadeInRight animate__repeat-3 animate__slow" src="img/arrows-left.svg" alt="arrows-left.svg"></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right animate__animated animate__fadeInLeft animate__repeat-3 animate__slow" src="img/arrows-right.svg" alt="arrows-right.svg"></img>',
+  })
+
+  $('.surf__popup svg').map((index, elem) => {
+    console.log(arguments)
+    ++index;
+    $(`.${elem.className.animVal}`).mouseover(function () {
+      $(`.${'surf__popup-container'+index}, .${'slider-item__info-text'+index}`).css({
+        'opacity' : '1'
+      })
+    })
+    $(`.${elem.className.animVal}`).mouseout(function() {
+      $(`.${'surf__popup-container'+index}, .${'slider-item__info-text'+index}`).css({
+        'opacity' : '0'
+      })
+    })
+  })
+
+
+
+
 });
